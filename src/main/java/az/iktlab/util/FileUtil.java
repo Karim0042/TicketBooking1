344ac5078473d6bc477b3f2687217c92d38cda8e
Karim <2KarimKarimov004@gmail.com>
2023-09-01 21:38:03 +0400

    intial

--- src/main/java/az/iktlab/util/FileUtil.java
+++ src/main/java/az/iktlab/util/FileUtil.java
@@ -1,17 +1,33 @@
 package az.iktlab.util;
 
+import az.iktlab.dao.FlightRepository;
+import az.iktlab.dao.impl.FlightDaoImpl;
+import az.iktlab.dto.FlightDto;
 import az.iktlab.model.Flight;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
 
 public class FileUtil {
-    public List<Flight> readFromFile(String path) {
-        return null;
+    public void readFromFile(String path) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+
+        try {
+            FlightDto flightData = objectMapper.readValue(new File("C:\\Users\\Acer\\IdeaProjects\\finalApp\\docs\\flights.json"), FlightDto.class);
+
+
+            FlightRepository flightRepository = new FlightDaoImpl();
+            for (Flight flight : flightData.getFlights()) {
+                flightRepository.insertFlight(flight);
+            }
+
+            System.out.println("Data insertion completed successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+
 }
